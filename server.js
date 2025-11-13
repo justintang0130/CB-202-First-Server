e3a0fe6f65574f9f9e4ca1cdb598a020173e2812
Justin Tang <justintang0130@gmail.com>
2025-11-13 23:33:51 +0000

    initial

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Hello from the Express Server!")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
